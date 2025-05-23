56cc87c9fdd633bec3500c11a31fadc7527b2634
Rella400 <2722723@students.wits.ac.za>
2025-05-23 20:44:01 +0200

    Added email input to footer

--- js/universal.js
+++ js/universal.js
@@ -1,3 +1,13 @@
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+
+function checkEmail() {
+  const email = document.getElementById("email").value;
+  if (!email.includes("@")) {
+    alert("Please enter a valid email.");
+  } else {
+    alert("Thank you!");
+  }
+}
