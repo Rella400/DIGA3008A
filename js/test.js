e872a8bb73ba1e4b6cf01328ad44136e4e52e787
Rella400 <2722723@students.wits.ac.za>
2025-06-13 19:01:03 +0200

    Added Essays and Blogs

--- js/test.js
+++ js/test.js
@@ -65,3 +65,15 @@ function exercise5(){
         numCheck = true
     }
 }
+
+
+//TEST BUTTON
+
+function updateName() {
+  const name = prompt("Enter a new name");
+  button.textContent = `Player 1: ${name}`;
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", updateName);
